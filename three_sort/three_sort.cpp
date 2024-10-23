7c62bb05c2a0f74f55a393d16161c05e2b530096
Ruslan Gafarov <gafarov-ruslan@internet.ru>
2024-10-23 22:27:53 +0300

    Реализованы функции слияния двух отсортированных массивов и сортировка слиянием + немного косметики

--- three_sort/three_sort.cpp
+++ three_sort/three_sort.cpp
@@ -17,7 +17,8 @@ void Bubble(int arr[], int size)
         }
     }
     cout << endl;
-
+    
+    //вывод отсортированного массива
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
 }
@@ -39,6 +40,7 @@ void Choice(int arr[], int size)
     }
     cout << endl;
 
+    //вывод отсортированного массива
     for (int i = 0; i < size; i++)    
         cout << arr[i] << " "; 
 }
@@ -55,9 +57,9 @@ void Insert(int arr[], int size)
     }
     cout << endl;
 
+    //вывод отсортированного массива
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
-    
 }
 //быстрая сортировка (сортировка Хоара)
 void Quick(int arr[], int left, int right) {
@@ -80,10 +82,70 @@ void Quick(int arr[], int left, int right) {
     //рекурсивные вызовы для обеих частей
     if (left < j) Quick(arr, left, j);
     if (i < right) Quick(arr, i, right);
-}
-//сортировка слиянием 
-void Merge() {
 
+}
+//функция слияния двух отсортированных массивов 
+void Merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    //создаем временные массивы
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    //копируем данные во временные массивы
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    //индексы для перебора временных массивов и основного массива
+    int i = 0; int j = 0; int k = left;
+
+    //объединение временных массивов обратно в основной arr
+    while (i < n1 && j < n2) {
+
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    //копируем оставшиеся элементы L[] если есть
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    //копируем оставшиеся элементы R[] если есть
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    //очистка временных массивов
+    delete[] R;
+    delete[] L;
+}
+//сортировка слиянием
+void MergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        //сортировка первой и второй половины
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid + 1, right);
+
+        //сливаем отсортированные массивы
+        Merge(arr, left, mid, right);
+    }
 }
 //сортировка кучей (пирамидальная)
 void Heap() {
@@ -116,8 +178,12 @@ int main()
     srand(time(NULL));
     int size;
     int chose;
-    cout << "Здравствуйте, выберите тип сортировки: 1 - Пузырьковая, 2 - Выбором, 3 - Вставками" << endl;
+
+    cout << "Здравствуйте, выберите тип сортировки:\n";
+    cout << "1 - Пузырьковая, 2 - Выбором, 3 - Вставками\n";
+    cout << "4 - Быстрая, 5 - Слиянием";
     cin >> chose;
+
     cout << "Введите размер массива" << endl;
     cin >> size;
     int* arr = new int[size];
@@ -126,43 +192,66 @@ int main()
     {
     case 1:
         cout << "Сортировка пузырьком:\n";
+
         MassGen(arr, size);
         cout << " - сгенерированный массив\n";
+
         Bubble(arr, size);
         cout << " - отсортированный массив\n";
+
         break;
 
     case 2:
         cout << "Сортировка выбором:\n";
+
         MassGen(arr, size);
         cout << " - сгенерированный массив\n";
+
         Choice(arr, size);
         cout << " - отсортированный массив\n";
+
         break;
 
     case 3:
         cout << "Сортировка вставками:\n";
+
         MassGen(arr, size);
-        cout << " - сгенерированный массив" << endl;
-        cout << endl;
+        cout << " - сгенерированный массив\n";
+
         Insert(arr, size);
-        cout << " - отсортированный массив" << endl;
+        cout << " - отсортированный массив\n";
+
         break;
 
     case 4: 
-        cout << "Быстрая сортировка: " << endl;
+        cout << "Быстрая сортировка:\n";
+
         MassGen(arr, size);
         cout << " - сгенерированный массив\n";
-        cout << endl;
+
         Quick(arr, 0, size - 1);
         cout << " - отсортированный массив\n";
+
         for (int i = 0; i < size; i++)
-        {
-            cout << arr[i] << " ";
-        }
+            cout << arr[i] << " "; 
         cout << endl;
+
         break;
 
+    case 5:
+        cout << "Сортировка слиянием:\n";
+
+        MassGen(arr, size);
+        cout << " - сгенерированный массив\n";
+       
+        MergeSort(arr, 0, size - 1);
+        cout << " - отсортированный массив\n";
+
+        for (int i = 0; i < size; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+
+        break;
     }
     
 
